3c8eb9ff431c3d30e9f4edc598aa814930857e97
Pandit upase <panditupase1@gmail.com>
2026-01-17 19:27:43 +0530

    MANUAL EXCEPTION
    CREATING MANUAL EXCEPTION

---
+++ Exception Handling/manualexe.java
@@ -0,0 +1,16 @@
+public class manualexe {
+    
+    public static void main(String[] args) {
+        
+        int age =20;
+
+        if(age<18)
+        {
+            throw new ArithmeticException("Not eligible to vote");
+        }
+        else
+        {
+            System.out.println("Eligible for vote");
+        }
+    }
+}
