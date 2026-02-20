a5e78e4b674a2b6ef3e5cc8aa3d20217a8612692
Pandit upase <panditupase1@gmail.com>
2026-02-20 22:27:15 +0530

    Update list to include 32 for filtering

--- Java 8/firstgreater.java
+++ Java 8/firstgreater.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public class firstgreater {
     
      public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(100,49,27,87,27);
+        List<Integer> list = Arrays.asList(100,49,27,87,32,27);
 
         Optional<Integer> result = list.stream()
                                        .filter(n -> n > 50)
