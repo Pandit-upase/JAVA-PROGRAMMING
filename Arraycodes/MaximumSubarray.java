3ff34a16420a4d7663fa84aa94ab798a58786733
Pandit upase <panditupase1@gmail.com>
2026-02-27 23:24:13 +0530

    .

--- Arraycodes/MaximumSubarray.java
+++ Arraycodes/MaximumSubarray.java
@@ -1,6 +1,6 @@
 public class MaximumSubarray {
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, ,6};
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, ,6,7};
 
         int maxSoFar = arr[0];
         int currentSum = arr[0];
