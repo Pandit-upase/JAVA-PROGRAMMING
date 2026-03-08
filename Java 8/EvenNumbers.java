f3bd26ccbf25c6553682ee5534600705fca871f5
Pandit upase <panditupase1@gmail.com>
2026-03-08 22:29:56 +0530

    .

---
+++ Java 8/EvenNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumbers {
+    
+    public static void main(String[] args) {
+        
+        List<Integer>numbers=Arrays.asList(1,2,3,4,5,6);
+
+        List<Integer>evenumbers=
+                     numbers.stream()
+                     .filter(n ->n%2==0)
+                     .collect(Collectors.toList());
+
+                     System.out.println(evenumbers);
+    }
+}
