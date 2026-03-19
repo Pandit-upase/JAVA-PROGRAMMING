eeb19b357569b3a7999a71c7fa8a12ca274563f5
Pandit upase <panditupase1@gmail.com>
2026-03-19 23:39:31 +0530

    Change thread output message from 'Thread' to 'Threadd'

--- Multithreading/startrun.java
+++ Multithreading/startrun.java
@@ -1,7 +1,7 @@
 public class startrun {
     
     public static void main(String[] args) {
-        Thread t = new Thread(() -> System.out.println("Thread"));
+        Thread t = new Thread(() -> System.out.println("Threadd"));
         //t.run();   // executes in main thread
         //t.start(); // executes in new thread
     }
